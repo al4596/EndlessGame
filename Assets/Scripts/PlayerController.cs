4b7eee82805f0aa12ba0e59e2cd1777b7185142d
Danny Lin <danny.lin@nyu.edu>
2016-04-28 13:24:38 -0400

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+	private Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);	
+		}
+	}
+}
